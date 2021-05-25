055f0d65631a9450ee3d50f7bfe713517fd685cb
Kedakai <nicoleteeth@ucla.edu>
2021-05-25 16:39:38 -0700

    revise fetch request

--- public/javascripts/fetch.js
+++ public/javascripts/fetch.js
@@ -1,5 +1,8 @@
-var searchButton = document.getElementById("submit");
-searchButton.addEventListener("click", fetchApi);
+var searchButton = document.querySelector("#submit");
+searchButton.addEventListener("click", function () {
+    console.log("button clicked!");
+    fetchApi();
+});
 
 function fetchApi() {
 
@@ -9,31 +12,52 @@ function fetchApi() {
 
     if (fetchOption === "city") {
         fetchUrl = "https://prescriptiontrails.org/api/filter/?by=city&city=" + searchWord + "&offset=0&count=50";
+
     }
     else if(fetchOption === "zip" && fetchOption.length == 5) {
         fetchUrl = "https://prescriptiontrails.org/api/filter/?zip=" + searchWord + "&by=zip&offset=0&count=50";
+
     }
     else if(fetchOption === "name") {
         fetchUrl = "https://prescriptiontrails.org/api/filter/?name=" + searchWord + "&by=name&offset=0&count=50";
+
+
     }
 
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+    console.log(fetchOption + " " + searchWord);
+    console.log(fetchUrl);
 
-    var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
-    };
-    
-    return fetch(fetchUrl, requestOptions)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-        var test = data[0].name;
-        console.log("fetching trails");
-        console.log(test)
+    fetch(fetchUrl).then(function(response) {
+        console.log("fetch!!!");
+        // request was successful
+        if (response.ok) {
+          response.json().then(function(data) {
+            console.log(data);
+          });
+        }
+        else {
+          alert("There was a problem with your request!");
+        }
     });
+
+    // var myHeaders = new Headers();
+    // myHeaders.append("Content-Type", "application/json");
+
+    // var requestOptions = {
+    //   method: 'GET',
+    //   headers: myHeaders,
+    //   redirect: 'follow'
+    // };
+    
+    // return fetch(fetchUrl, requestOptions)
+    // .then(function(response) {
+    //   return response.json();
+    // })
+    // .then(function(data) {
+    //     var test = data[0].name;
+    //     console.log(data);
+    //     console.log("fetching trails");
+    //     console.log(test)
+    // });
 
 };
