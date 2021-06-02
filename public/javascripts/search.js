4621df5902b1e235a5cc36025d3d7587f6ab6165
unknown <raytakemura@gmail.com>
2021-06-02 00:13:16 -0700

    allow user to search with zip and empty search

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -8,17 +8,17 @@ function search() {
     var searchOption = $('#dropdown').val();
     var searchWord = $('#input').val();
 
+    if (searchWord === '') return;
     if (searchOption === "city") {
-        document.location.replace('trail/city/' + searchWord);
 
-    }
-    else if(searchOption === "zip" && searchOption.length == 5) {
-        document.location.replace('trail/zip/' + searchWord);
+        document.location.replace('trail/city/' + searchWord);
 
     }
     else if(searchOption === "name") {
         document.location.replace('trail/name/' + searchWord);
     }
-
+    else {
+        document.location.replace('trail/zip/' + searchWord);
+    }
     console.log(searchOption + " " + searchWord);
 };
